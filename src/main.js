6de7e3998eda159127b34ecb62f1938047c12a89
Olga Garkusha <olga.garkusha.uk@gmail.com>
2023-07-01 13:04:20 +0200

    added inject

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { inject } from '@vercel/analytics'
 
 import './assets/fonts/sf-pro-display-fonts/fonts.scss'
 import './assets/scss/parts/_reset.scss'
 import './assets/scss/parts/hamburgers.css'
 import './assets/scss/style.scss'
 
+inject()
 const app = createApp(App)
 
 app.use(router)
